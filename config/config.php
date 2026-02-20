4e62801995f81757f59cf7697e6399fb4b15a38a
LiamGael2023 <cesar8420@gmail.com>
2026-02-20 12:06:24 -0500

    Seguridad: eliminando archivos de configuración del repositorio

--- config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Esto detecta automáticamente si entras por localhost o por la IP 10.0.100.252
-$host = $_SERVER['HTTP_HOST']; 
-$protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? "https://" : "http://";
-
-define('BASE_URL', $protocol . $host . '/gestionTI');
-// Esto ayuda a que las sesiones se mantengan en el dominio correcto
-ini_set('session.cookie_domain', $_SERVER['HTTP_HOST']); 
-?>
