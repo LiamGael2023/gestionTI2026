bbde4818c8f03dfd25545d4ce3ab45ca03a014df
crhixtian <crhistian.villantoy@hotmail.com>
2026-02-24 11:47:50 -0500

    test commit: index.php

--- modules/adquisiciones/views/index.php
+++ modules/adquisiciones/views/index.php
@@ -1,2 +1,2 @@
 <div class='page-header'><div class='container-xl'><h2 class='page-title'>Adquisición de Equipos</h2></div></div>
-<div class='page-body'><div class='container-xl'><div class='card'><div class='card-body'>Bienvenido al sistema de Adquisición de Equipos.</div></div></div></div>
+<div class='page-body'><div class='container-xl'><div class='card'><div class='card-body'>Sistema de Adquisición de Equipos.</div></div></div></div>
